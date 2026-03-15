f23fdd78b6187929c3ac805819134c7f06ecaa7f
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2026-03-15 12:23:18 +0100

    Initial Commit

---
+++ SwiftMachine/SwiftMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMachineApp.swift
+//  SwiftMachine
+//
+//  Created by Thibault Wittemberg on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
